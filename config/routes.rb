bb53ae83b54b6ebfa0833dd9ebd7efd800abbe36
Byron Arocha <bjarocha911@gmail.com>
2016-07-20 20:52:28 +0000

    Add create action for user-stock relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  resources :user_stocks, except: [:edit, :update, :show]
   root 'welcome#index'
   
   get 'my_portfolio', to: 'users#my_portfolio'
